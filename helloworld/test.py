20568c9c75ff0fe371ccc656decd511ed9abc278
HelloKugou <chenzezhen123@163.com>
2017-11-16 02:29:11 +0800

    test

--- helloworld/test.py
+++ helloworld/test.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import platform
+
 def main():
     print('hello world python')
+    print('cur system: %s' % platform.system())
 
 if __name__ == '__main__':
     main()
